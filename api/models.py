39196d97beba8f6495e540066ef9fbda81a161f2
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-16 16:22:41 +0500

    test model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+   name = models.CharField(max_length=30)
+   sound = models.CharField(max_length=50)
+
+   def speak(self):
+       return f'The {self.name} says "{self.sound}"'
+
+   def __str__(self):
+       return self.name
+
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Animal
 
-# Create your tests here.
+
+class AnimalTestCase(TestCase):
+   def setUp(self):
+       self.lion = Animal.objects.create(name="lion", sound="roar")
+       self.cat = Animal.objects.create(name="cat", sound="meow")
+
+
+   def test_animals_can_speak(self):
+       """Animals that can speak are correctly identified"""
+
+       self.assertEqual(self.lion.speak(), 'The lion says "roar"')
+       self.assertEqual(self.cat.speak(), 'The cat says "meow"')
+
